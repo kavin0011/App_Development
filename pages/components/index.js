21e977590976934241816e07eb8ea679d9750329
kavin0011 <150165983+kavin0011@users.noreply.github.com>
2024-07-29 09:39:14 +0530

    Delete pages/components directory

--- pages/components/index.js
+++
@@ -1,7 +0,0 @@
-import Discover from './NavBar/Discover';
-import HelpCenter from './NavBar/HelpCenter'
-import Notification from './NavBar/Notification';
-import Profile from './NavBar/Profile';
-import SideBar from './NavBar/SideBar'
-
-export {Discover,HelpCenter,Profile,Notification,SideBar};
